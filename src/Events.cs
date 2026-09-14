650e3248d7927e223ead8e4bb6429cd30544dacf
Matthew Garcia <matt.e.garcia@gmail.com>
2026-09-14 15:25:14 -0500

    added "using Landis.Library.UniversalCohorts" for ease of using DeathEventArgs; not entirely sure why we have both Events.cs/CohortDied and PlugIn.cs/DeathEvent.

--- src/Events.cs
+++ src/Events.cs
@@ -1,12 +1,13 @@
 ﻿using Landis.Core;
 using Landis.Library.PnETCohorts;
+using Landis.Library.UniversalCohorts;
 using Landis.SpatialModeling;
 
 namespace Landis.Extension.Succession.BiomassPnET.DisturbanceReductions
 {
     class Events
     {
-        public static void CohortDied(object sender, Landis.Library.UniversalCohorts.DeathEventArgs eventArgs)
+        public static void CohortDied(object sender, UniversalCohorts.DeathEventArgs eventArgs)
         {
             ExtensionType disturbanceType = eventArgs.DisturbanceType;
         }
